9a01b036dddefb549582896003b6d892508544ff
Eamonmca <emcandrew12@gmail.com>
2023-01-18 16:14:09 +0000

    add VAE models

--- sc_generative_demo/Models/VariationalEncoder.py
+++ sc_generative_demo/Models/VariationalEncoder.py
@@ -24,7 +24,7 @@ class VariationalEncoder(nn.Module):
 
         # input layer
         layers.append(nn.Linear(self.input_size, self.hidden_sizes[0]))
-        layers.append(nn.LeakyReLU(0.2))
+        layers.append(nn.ReLU(0.2))
         if self.dropout > 0:
             layers.append(nn.Dropout(p=self.dropout))
 
@@ -33,7 +33,7 @@ class VariationalEncoder(nn.Module):
             layers.append(nn.Linear(self.hidden_sizes[i-1], self.hidden_sizes[i]))
             if self.use_norm:
                 layers.append(nn.InstanceNorm1d(self.hidden_sizes[i]))
-            layers.append(nn.LeakyReLU(0.2))
+            layers.append(nn.ReLU(0.2))
             if self.dropout > 0:
                 layers.append(nn.Dropout(p=self.dropout))
             
@@ -41,8 +41,14 @@ class VariationalEncoder(nn.Module):
       
         # create the model using Sequential
         self.model = nn.Sequential(*layers)
+        self.mu_layer = nn.Linear(self.hidden_sizes[-1], self.latent_size)
+        self.logvar_layer = nn.Linear(self.hidden_sizes[-1], self.latent_size)
+       
+        
 
     def forward(self, x):
         x = self.model(x)
+        mu = self.mu_layer(x)
+        logvar = self.logvar_layer(x)
         return x
 
