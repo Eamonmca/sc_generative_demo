87804f446c63852c384e010fdfd03dc5164585be
Eamonmca <emcandrew12@gmail.com>
2023-01-25 20:22:58 +0000

    Added the VAEGAN model with Neagtive binomial as Latent variable

--- sc_generative_demo/Models/VAEGAN_NEG_BI.py
+++ sc_generative_demo/Models/VAEGAN_NEG_BI.py
@@ -42,6 +42,7 @@ class VAEGAN_NEG_BI(nn.Module):
         z = self.reparameterize(mu, logvar)
         h_r = self.decoder_r(z)
         h_p = self.decoder_p(z)
+        h_r = F.relu(h_r)
         h_p = F.relu(h_p)
         y_hat = self.classifier(z)
         x_hat = NegativeBinomial(h_r, logits = h_p).sample()
