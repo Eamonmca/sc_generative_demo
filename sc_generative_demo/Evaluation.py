6c5d2a21ff859c5a2ab39716f426f7dbfaed7c38
Eamonmca <emcandrew12@gmail.com>
2023-01-30 16:14:20 +0000

    Added the VAEGAN model with Neagtive binomial as Latent variable

--- sc_generative_demo/Evaluation.py
+++ sc_generative_demo/Evaluation.py
@@ -96,7 +96,7 @@ def decode_embeddings_VAEGAN_NEG_BI(VAEGAN, embeddings, l, device, norm = False)
                 x_hat, h_r, h_p = VAEGAN.decode(batch[None], l, train = False)
                 x_hat = torch.squeeze(x_hat)
             else:
-                x_hat, h_r, h_p, l = VAEGAN.decode(batch, l)
+                x_hat, h_r, h_p, l = VAEGAN.decode(batch, l, train = False)
             embeddings_list.append(x_hat.cpu().numpy())
         embeddings_list = np.array(embeddings_list)
         return embeddings_list
