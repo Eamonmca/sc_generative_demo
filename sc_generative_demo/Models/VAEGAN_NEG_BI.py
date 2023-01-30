21779a2781d05133d7a7d6112e5e7af4284c69b6
Eamonmca <emcandrew12@gmail.com>
2023-01-30 17:46:17 +0000

    Added the VAEGAN model with Neagtive binomial as Latent variable

--- sc_generative_demo/Models/VAEGAN_NEG_BI.py
+++ sc_generative_demo/Models/VAEGAN_NEG_BI.py
@@ -49,7 +49,7 @@ class VAEGAN_NEG_BI(nn.Module):
         mu_z, logvar_z = self.encoder_z(x)
         
         l = self.reparameterize(mu_l, logvar_l)
-        l = torch.clamp(l, min=12)
+        l = torch.clamp(l, max=12)
         z = self.reparameterize(mu_z, logvar_z)
         
         x_hat, h_r, h_p = self.decode(z,l)
